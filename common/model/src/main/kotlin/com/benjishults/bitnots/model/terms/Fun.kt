b59f027d861876ad1fb5b6e3d9abceee853f6645
bshults <benji.shults@windlogics.com>
2017-07-05 14:03:13 -0500

    still Eclipse not building

--- common/model/src/main/kotlin/com/benjishults/bitnots/model/terms/Fun.kt
+++ common/model/src/main/kotlin/com/benjishults/bitnots/model/terms/Fun.kt
@@ -11,84 +11,84 @@ fun Const(name: String) = Fn(name, 0)
 
 fun Fn(name: String, arity: Int = 1) = FunctionConstructor.intern(name, arity)
 
-class Function private constructor(name: Fun, val arguments: Array<Term>) : Term(name) {
+class Function private constructor(name: FunctionConstructor, val arguments: Array<Term>) : Term(name) {
 
-	class FunctionConstructor private constructor(name: String, arity: Int = 0) : TermConstructor(name) {
-		companion object inner : InternTable<Fun>({ name -> Fun(name) })
+    class FunctionConstructor private constructor(name: String, val arity: Int = 0) : TermConstructor(name) {
+        companion object inner : InternTable<Fun>({ name -> FunctionConstructor(name) })
 
-		operator fun invoke(arguments: Array<Term>) = Function(this, arguments)
-		operator fun invoke(argument: Term) =
-				Function(this, arrayOf(argument))
+        operator fun invoke(arguments: Array<Term>) {
+            check(arguments.)
+            Function(this, arguments)
+        }
 
-		operator fun invoke(arg1: Term, arg2: Term) =
-				Function(this, arrayOf(arg1, arg2))
+        operator fun invoke(argument: Term) = Function(this, arrayOf(argument))
 
-		operator fun invoke(arg1: Term, arg2: Term, arg3: Term) =
-				Function(this, arrayOf(arg1, arg2, arg3))
+        operator fun invoke(arg1: Term, arg2: Term) = Function(this, arrayOf(arg1, arg2))
 
-		operator fun invoke(arg1: Term, arg2: Term, arg3: Term, arg4: Term) =
-				Function(this, arrayOf(arg1, arg2, arg3, arg4))
+        operator fun invoke(arg1: Term, arg2: Term, arg3: Term) = Function(this, arrayOf(arg1, arg2, arg3))
 
-		operator fun invoke(arg1: Term, arg2: Term, arg3: Term, arg4: Term, arg5: Term) =
-				Function(this, arrayOf(arg1, arg2, arg3, arg4, arg5))
-	}
+        operator fun invoke(arg1: Term, arg2: Term, arg3: Term, arg4: Term) = Function(this, arrayOf(arg1, arg2, arg3, arg4))
 
-	override fun contains(variable: Variable): Boolean = arguments.any { it.contains(variable) }
+        operator fun invoke(arg1: Term, arg2: Term, arg3: Term, arg4: Term, arg5: Term) =
+                Function(this, arrayOf(arg1, arg2, arg3, arg4, arg5))
+    }
 
-	override fun unify(other: Term, sub: Substitution): Substitution {
-		if (other is Function && other.cons === cons) {
-			return sub.compose(arguments.foldIndexed(EmptySub as Substitution) { i, s, t ->
-				t.unify(other.applySub(s).arguments[i].applySub(s), s).let {
-					when (it) {
-						NotUnifiable -> return NotUnifiable
-						else -> it
-					}
-				}
-			})
-		} else if (other is FreeVariable)
-			return sub.compose(Sub(other.to(this)))
-		else
-			return NotUnifiable
-	}
+    override fun contains(variable: Variable): Boolean = arguments.any { it.contains(variable) }
 
-	override fun getFreeVariables(): Set<FreeVariable> =
-			arguments.fold(emptySet<FreeVariable>()) { s, t -> s.union(t.getFreeVariables()) }
+    override fun unify(other: Term, sub: Substitution): Substitution {
+        if (other is Function && other.cons === cons) {
+            return sub.compose(arguments.foldIndexed(EmptySub as Substitution) { i, s, t ->
+                t.unify(other.applySub(s).arguments[i].applySub(s), s).let {
+                    when (it) {
+                        NotUnifiable -> return NotUnifiable
+                        else -> it
+                    }
+                }
+            })
+        } else if (other is FreeVariable)
+            return sub.compose(Sub(other.to(this)))
+        else
+            return NotUnifiable
+    }
 
-	override fun getFreeVariablesAndCounts(): MutableMap<FreeVariable, Int> =
-			arguments.fold(mutableMapOf<FreeVariable, Int>()) { s, t ->
-				t.getFreeVariablesAndCounts().entries.forEach { (v, c) ->
-					s.get(v)?.also {
-						s.put(v, c + it)
-					} ?: s.put(v, c)
-				}
-				s
-			}
+    override fun getFreeVariables(): Set<FreeVariable> =
+            arguments.fold(emptySet<FreeVariable>()) { s, t -> s.union(t.getFreeVariables()) }
 
-	override fun applySub(substitution: Substitution): Function {
-		return Fun(cons.name, arguments.map { it.applySub(substitution) }.toTypedArray())
-	}
+    override fun getFreeVariablesAndCounts(): MutableMap<FreeVariable, Int> =
+            arguments.fold(mutableMapOf<FreeVariable, Int>()) { s, t ->
+                t.getFreeVariablesAndCounts().entries.forEach { (v, c) ->
+                    s.get(v)?.also {
+                        s.put(v, c + it)
+                    } ?: s.put(v, c)
+                }
+                s
+            }
 
-	override fun getVariablesUnboundExcept(boundVars: List<Variable>): Set<Variable> {
-		val value = mutableSetOf<Variable>()
-		arguments.map { value.addAll(it.getVariablesUnboundExcept(boundVars)) }
-		return value.toSet()
-	}
+    override fun applySub(substitution: Substitution): Function {
+        return cons(arguments.map { it.applySub(substitution) }.toTypedArray())
+    }
 
-	override fun toString() = "(${cons.name}${if (arguments.size == 0) "" else " "}${arguments.joinToString(" ")})"
+    override fun getVariablesUnboundExcept(boundVars: List<Variable>): Set<Variable> {
+        val value = mutableSetOf<Variable>()
+        arguments.map { value.addAll(it.getVariablesUnboundExcept(boundVars)) }
+        return value.toSet()
+    }
 
-	override fun equals(other: Any?): Boolean {
-		if (other === null) return false
-		if (other::class === this::class) {
-			if ((other as Function).arguments.size == this.arguments.size) {
-				for (index in 0..this.arguments.size - 1) {
-					if (other.arguments[index] != this.arguments[index])
-						return false
-				}
-				return true
-			}
-		}
-		return false
-	}
+    override fun toString() = "(${cons.name}${if (arguments.size == 0) "" else " "}${arguments.joinToString(" ")})"
+
+    override fun equals(other: Any?): Boolean {
+        if (other === null) return false
+        if (other::class === this::class) {
+            if ((other as Function).arguments.size == this.arguments.size) {
+                for (index in 0..this.arguments.size - 1) {
+                    if (other.arguments[index] != this.arguments[index])
+                        return false
+                }
+                return true
+            }
+        }
+        return false
+    }
 }
 
 //fun Fun(name: String, arguments: Array<Term> = emptyArray()): Function = FunctionConstructor.intern(name)(arguments)
