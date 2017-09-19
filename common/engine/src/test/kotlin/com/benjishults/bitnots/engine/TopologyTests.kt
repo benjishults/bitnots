3ac114c95a8a88b0351fc501833f7960ec0fe40f
bshults <benji.shults@windlogics.com>
2017-09-19 16:05:01 -0500

    more tests

--- common/engine/src/test/kotlin/com/benjishults/bitnots/engine/TopologyTests.kt
+++ common/engine/src/test/kotlin/com/benjishults/bitnots/engine/TopologyTests.kt
@@ -5,27 +5,32 @@ import com.benjishults.bitnots.engine.proof.FolTableauNode
 import com.benjishults.bitnots.inference.rules.SignedFormula
 import com.benjishults.bitnots.inference.rules.createSignedFormula
 import com.benjishults.bitnots.model.formulas.Formula
+import com.benjishults.bitnots.model.formulas.propositional.And
+import com.benjishults.bitnots.model.formulas.propositional.Implies
 import com.benjishults.bitnots.tptp.files.TptpDomain
 import com.benjishults.bitnots.tptp.files.TptpFileFetcher
 import com.benjishults.bitnots.tptp.files.TptpFormulaForm
 import com.benjishults.bitnots.tptp.parser.FofAnnotatedFormula
 import com.benjishults.bitnots.tptp.parser.TptpParser
 import org.junit.Assert
-import org.junit.Test
 import org.junit.Ignore
+import org.junit.Test
 
-class PlaistedTests {
+class TopologyTests {
 
     @Test
-    @Ignore
-    fun testFofPlaisted() {
+//    @Ignore
+    fun testFofGroups() {
         try {
-            val path = TptpFileFetcher.findProblemFile(TptpDomain.PLA, TptpFormulaForm.FOF, 34, 1)
+            val path = TptpFileFetcher.findProblemFile(TptpDomain.TOP, TptpFormulaForm.FOF, 22, 1)
 
             TptpParser.parseFile(path).let { tptp ->
-                FolTableau(FolTableauNode(ArrayList<SignedFormula<Formula<*>>>().also {
-                    it.add((tptp.inputs.first() as FofAnnotatedFormula).formula.createSignedFormula())
-                }, null)).also { tableau ->
+                FolTableau(FolTableauNode(mutableListOf<SignedFormula<Formula<*>>>(
+                        Implies(And(*tptp.inputs.dropLast(1).map {
+                            (it as FofAnnotatedFormula).formula
+                        }.toTypedArray()),
+                                (tptp.inputs.last() as FofAnnotatedFormula).formula).createSignedFormula()),
+                        null)).also { tableau ->
                     while (true) {
                         if (tableau.isClosed())
                             break
@@ -44,5 +49,5 @@ class PlaistedTests {
 
 fun main(args: Array<String>) {
     System.setProperty("config", "/home/bshults/repos/benjishults/bitnots/config")
-    PlaistedTests().testFofPlaisted()
+    TopologyTests().testFofGroups()
 }
