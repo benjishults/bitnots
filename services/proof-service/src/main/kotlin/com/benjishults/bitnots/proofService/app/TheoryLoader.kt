042ddfaa8070662cf2341ef92781ffe83d9e07e0
Benji Shults <benjishults@gmail.com>
2019-12-25 13:28:56 -0600

    load file

--- services/proof-service/src/main/kotlin/com/benjishults/bitnots/proofService/app/TheoryLoader.kt
+++ services/proof-service/src/main/kotlin/com/benjishults/bitnots/proofService/app/TheoryLoader.kt
@@ -2,18 +2,25 @@ package com.benjishults.bitnots.proofService.app
 
 import javafx.beans.property.SimpleStringProperty
 import tornadofx.*
+import java.io.File
 
 class TheoryLoader : View("Load a Problem") {
-    val selectedFile = SimpleStringProperty("file").apply { value = "first" }
-    override val root = vbox {
-        combobox(selectedFile, listOf("first", "second"))
-        button("Load") {
-            action {
-                openInternalWindow(object : View("Selected") {
-                    override val root = label("selected: ${selectedFile.value}")
+    val selectedFile = SimpleStringProperty("file")
+    val fileContents = SimpleStringProperty()
+    lateinit var file: File
 
-                })
-            }
+    init {
+        file = chooseFile("Select", arrayOf()) {
+            initialDirectory = File(System.getProperty("user.home"))
+        }[0]
+        selectedFile.value = file.absolutePath
+        fileContents.value = file.inputStream().use {
+            String(it.readAllBytes())
         }
+    }
+
+    override val root = vbox {
+        label(selectedFile)
+        textarea(fileContents)
     }
 }
